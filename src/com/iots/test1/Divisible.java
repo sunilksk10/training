2729d651ffeb9a9ed68d0ee2571f4630145cb548
Sunil <Sunil@10.0.0.7>
2016-06-06 23:28:07 -0500

    Added divisible program

---
+++ src/com/iots/test1/Divisible.java
@@ -0,0 +1,44 @@
+package com.iots.test1;
+
+public class Divisible {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		
+		 {   // Every program must have main()
+		    for(int i = 1; i <= 50; i++) {                    // count from 1 to 100
+		      if (((i % 3) == 0) && ((i % 5) == 0))            // A multiple of both?
+		        System.out.println("fizzbuzz");    
+		      else if ((i % 3) == 0) System.out.println("fizz"); // else a multiple of 3?
+		      else if ((i % 5) == 0) System.out.println("buzz"); // else a multiple of 5?
+		      else System.out.println(i);                        // else just print it
+		      //System.out.println(" "); 
+		    }
+		    System.out.println();
+		  }
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		int i;
+
+		for (i = 1; i <= 50; i++) {
+			if ((i % 3) == 0) {
+				System.out.println("Deepika");
+			}
+			if ((i % 5) == 0) {
+				System.out.println("Padukone");
+			}
+			if ((i % 3) == 0 && (i % 5) == 0) {
+				System.out.println("Deepika Padukone");
+			}
+		} */
+	}
+
